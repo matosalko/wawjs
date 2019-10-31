8e0cb037495ca1b72922de35a54c23d7e81facdd
matosalko <salko.matej@gmail.com>
2019-10-31 14:26:51 +0100

    vypracovnaie ulohy 1 a 2 z cvika

--- 06-cvicenie/ulohy/src/saveSomewhere1.js
+++ 06-cvicenie/ulohy/src/saveSomewhere1.js
@@ -1,8 +1,15 @@
- const async = require("async");
- const fs = require("fs");
- module.exports = saveSomewhere;
+const async = require("async");
+const fs = require("fs");
+module.exports = saveSomewhere;
 
- function saveSomewhere(paths, data, cb) {
-   const tasks = //...
-   async./*whatMethod*/(tasks, cb);
- }
+function saveSomewhere(paths, data, cb) {
+ const tasks = paths.map(function(path) {
+   return (cb) => fs.writeFile(path, data, (err) => {
+     if(err) return cb(err);
+     cb(null,path);
+   })
+ });
+
+ async.tryEach(tasks, cb);
+}
+
--- 06-cvicenie/ulohy/src/writeTempFile1.js
+++ 06-cvicenie/ulohy/src/writeTempFile1.js
@@ -6,10 +6,20 @@ const path = require("path");
 
 
 function writeTempFile(fileName, ...args) {
-  // just hints:
-  // const cb = args.pop();
-  // const tempDir = path.join(os.tmpdir(), `${process.pid}-`);
-  // fs.mkdtemp(tempDir, (err, folder) => {  
-  // fs.writeFile(tempFile, ...args, (err) => {
-         
+  const cb = args.pop();
+  const tempDir = path.join(os.tmpdir(), `${process.pid}-`);
+
+    fs.mkdtemp(tempDir, (err, tempDir) => {
+      if(err) return cb(err);
+      const tempFile = path.resolve(tempDir,fileName);
+      try{
+      fs.writeFile(tempFile,...args,(err)=>{
+        if(err) return cb(err);
+        
+        cb(null,tempFile);
+      })
+    }catch(err){
+      cb(err);
+    }
+    });      
 }
