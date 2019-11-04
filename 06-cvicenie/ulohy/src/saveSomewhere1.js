35e215fc3a22b024342fdf342a41939392d443d4
matosalko <salko.matej@gmail.com>
2019-11-04 19:22:21 +0100

    drobna uprava

--- 06-cvicenie/ulohy/src/saveSomewhere1.js
+++ 06-cvicenie/ulohy/src/saveSomewhere1.js
@@ -5,8 +5,7 @@ module.exports = saveSomewhere;
 function saveSomewhere(paths, data, cb) {
  const tasks = paths.map(function(path) {
    return (cb) => fs.writeFile(path, data, (err) => {
-     if(err) return cb(err);
-     cb(null,path);
+     cb(err,path);
    })
  });
 
